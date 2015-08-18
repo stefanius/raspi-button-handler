a3af3000ea33f4b995a3c2caf8dd5e8117b8b321
stefanius <KIT_kat_16>
2015-08-18 21:55:23 +0200

    make options in use

--- lib/listener.py
+++ lib/listener.py
@@ -14,41 +14,32 @@ parser.add_option("-s", "--scriptpath", dest="scriptpath", metavar="PATH",
                   help="Set the path with pinscripts")
 (options, args) = parser.parse_args()
 
-print options.pin
-
-pin = int(sys.argv[1])
-detect_bounce = 0
-
-if 2 in sys.argv:
-    detect_bounce = int(sys.argv[2])
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.IN)
+GPIO.setup(options.pin, GPIO.IN)
 active = 0
 
 
 def callback_function(channel):
     global active
-    global pin
-    global detect_bounce
+    global options
 
-    if (GPIO.input(pin) and not active):    #pin is pressed
-        print "Pin " + str(pin) + " is ON (rising event)"
+    if (GPIO.input(options.pin) and not active):    #pin is pressed
+        print "Pin " + str(options.pin) + " is ON (rising event)"
         active = 1
-    elif (not GPIO.input(pin) and active):
-        print "Pin " + str(pin) + " is OFF (falling event)"
+    elif (not GPIO.input(options.pin) and active):
+        print "Pin " + str(options.pin) + " is OFF (falling event)"
         active = 0
     else:
-        if detect_bounce:
+        if options.bounce:
             print "bounce"
 
 # when a changing edge is detected on port 25, regardless of whatever
 # else is happening in the program, the function my_callback will be run
-GPIO.add_event_detect(pin, GPIO.BOTH, callback=callback_function)
+GPIO.add_event_detect(options.pin, GPIO.BOTH, callback=callback_function)
 
 try:
-    print "When pressed, you'll see: Rising Edge detected on " + str(pin)
-    print "When released, you'll see: Falling Edge detected on " + str(pin)
+    print "When pressed, you'll see: Rising Edge detected on " + str(options.pin)
+    print "When released, you'll see: Falling Edge detected on " + str(options.pin)
     sleep(120)
     print "Listener is shutdown"
 
